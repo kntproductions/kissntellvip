83bdef235ccc59a31659a2057f628cb83602d611
KNTPRODUCTIONS <kntproductions@proton.me>
2025-05-07 20:50:01 -0500

    fixed site in config.mjs and link to embedded cams

--- src/controllers/videoPlayer.controller.LittleMiss.js
+++ src/controllers/videoPlayer.controller.LittleMiss.js
@@ -1,6 +1,7 @@
 const loader = document.querySelector(".heart__loader");
 const initializeModal = (iframe) => {
-  const embeddUrl = "https://chaturbate.com/fullvideo/?b=xxlittlemiss95xx&campaign=9r5I1&disable_sound=1&mobileRedirect=never&signup_notice=1&tour=ZQAI&track=embed";
+  const embeddUrl = "https://chaturbate.com/embed/xxlittlemiss95xx/?campaign=9r5I1&disable_sound=1&embed_video_only=0&join_overlay=1&mobileRedirect=auto&room=xxlittlemiss95xx&target=_parent&tour=ZQAI&track=embed";
+/* "https://chaturbate.com/fullvideo/?b=xxlittlemiss95xx&campaign=9r5I1&disable_sound=1&mobileRedirect=never&signup_notice=1&tour=ZQAI&track=embed" */
 
   iframe.setAttribute("src", embeddUrl);
 }
